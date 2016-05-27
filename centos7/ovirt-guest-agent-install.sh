075fd8cdf06a3a02fa36bd1604c0a20ed2d70fc1
Vinzenz Feenstra <vfeenstr@redhat.com>
2016-05-27 12:09:17 +0200

    Latest atomic updates

--- centos7/ovirt-guest-agent-install.sh
+++ centos7/ovirt-guest-agent-install.sh
@@ -2,19 +2,8 @@
 # Install scriptlet for ovirt/rhevm-guest-agent
 
 prefix=/host
-dbusdir=/etc/dbus-1/system.d
-ovirt_dbus=org.ovirt.vdsm.Credentials.conf
-
-if [ -e "$prefix/$dbusdir/$ovirt_dbus" ] ; then
-	echo "Backing up $prefix/$dbusdir/$ovirt_dbus to $prefix/$dbusdir/$ovirt_dbus.atomicnew"
-	cp $prefix/$dbusdir/$ovirt_dbus $prefix/$dbusdir/$ovirt_dbus.new
-fi
-
-echo "Installing $ovirt_dbus to $prefix/$dbusdir/"
-cp  $dbusdir/$ovirt_dbus $prefix/$dbusdir/
 
 # Add the ovirtagent user and group
-
 hostgroup=`nsenter --mount=/host/proc/1/ns/mnt -- getent group ovirtagent`
 echo "Host group is ${hostgroup}"
 if [ "${hostgroup}" == "" ]; then
